2bad3f8b403b22c80573b364d611e70f661183fb
damidia <adamidi.uw.edu>
2025-03-04 13:07:54 -0800

    Deployed draft2 to firebase

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,25 @@ import { BrowserRouter } from "react-router";
 
 import './index.css';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAWkcW34kyOD0r4JcIHoEbVILJsXFGcnEk",
+  authDomain: "project-sectionab-group4.firebaseapp.com",
+  projectId: "project-sectionab-group4",
+  storageBucket: "project-sectionab-group4.firebasestorage.app",
+  messagingSenderId: "996826751298",
+  appId: "1:996826751298:web:6b95e8c8a52f6b472be8db"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
