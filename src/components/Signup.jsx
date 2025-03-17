928677edb684a3428c96b29013dfe72216310d62
damidia <adamidi.uw.edu>
2025-03-17 16:48:01 -0700

    Removed comments not needed

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -25,32 +25,6 @@ function Signup({ users, setUsers }) {
     }
   };
 
-  //   const lowerEmail = email.toLowerCase();
-
-  //   const emailExists = users.some(user => user.email === lowerEmail);
-
-  //   if (emailExists) {
-  //     setError('Email already exists. Please login instead');
-  //   } else {
-  //     const nextUserId = users.length + 1;
-
-  //     const newUser = {
-  //       userId: nextUserId, // Assign unique ID to user
-  //       email: lowerEmail,
-  //       password,
-  //       firstName: firstName.toUpperCase(),
-  //       lastName: lastName.toUpperCase(),
-  //       locationState: locationState.toUpperCase(),
-  //       locationCity: locationCity.toUpperCase()
-  //     };
-  //     console.log([...users, newUser ]);
-  //     setUsers([...users, newUser]);
-  //     setError('');
-  //     alert('Signup successful! You can now login.');
-  //     setRedirect(true);
-  //   }
-  // };
-
   if (redirect) {
     return <Navigate to="/login" />
   }
@@ -66,18 +40,6 @@ function Signup({ users, setUsers }) {
 
         <label>Password:</label>
         <input type="password" value={password} onChange={(event) => setPassword(event.target.value)} required />
-
-        {/* <label>First Name:</label>
-        <input type="text" value={firstName} onChange={(event) => setFirstName(event.target.value)} required />
-
-        <label>Last Name:</label>
-        <input type="text" value={lastName} onChange={(event) => setLastName(event.target.value)} required />
-
-        <label>State:</label>
-        <input type="text" value={locationState} onChange={(event) => setLocationState(event.target.value.trim())} required />
-
-        <label>City:</label>
-        <input type="text" value={locationCity} onChange={(event) => setLocationCity(event.target.value.trim())} required /> */}
 
         <button type="submit" className="signup-button">Sign Up</button>
       </form>
